a85b46bbea20567421c0edb5d4a59454f95c8c37
rupaliswami <swamirupali27@gmail.com>
2026-01-02 18:15:06 +0530

    Logical DB Query and Compound index.

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,41 @@
+const mongooes = require('mongoose');
+
+
+const connectionRequestSchema = new mongooes.Schema({
+
+    fromUserId: {
+        type: mongooes.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type: mongooes.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["ignored", "interested", "accepted", "rejected"],
+            message: `{VALUE} is incorrect type`
+        }
+    }
+},
+{
+    timestamps: true
+});
+
+connectionRequestSchema.index({fromUserId: 1, toUserId: 1}); // Compound Index Asending Order.
+
+connectionRequestSchema.pre('save', function(next){
+    const connectionRequest = this;
+
+    // check if the fromUserId is same as toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error('Cannot send connection request to yourself!!')
+    }
+    next();
+})
+
+const ConnectionRequestModel = new mongooes.model('ConnectionModel', connectionRequestSchema);
+
+module.exports = ConnectionRequestModel;
--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,6 +1,53 @@
 const express = require('express');
 const requestRouter = express.Router();
 const User = require('../models/user');
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequest = require('../models/connectionRequest');
+
+requestRouter.post('/request/send/:status/:toUserId', userAuth, async(req,res)=> {
+    try{
+        
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const isAllowedStatus = ["ignored", "interested", "accepted", "rejected"];
+        if(!isAllowedStatus.includes(status)){
+            return res.status(400).json({"message": "Invalid status type!!" + status});
+        }
+
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            return res.status(404).json({message: "User does not exists!!"});
+        }
+
+        // If there is an existing ConnectionRequest
+        const existingRequest = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId: toUserId, toUserId: fromUserId}
+            ]
+        });
+
+        if(existingRequest){
+            return res.status(400).send({message: "Connection Request already exists!!"})
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+        const data = await connectionRequest.save();
+
+        return res.json({
+            message: "Connection Request sent successfully!!",
+            data
+        })
+    }catch(err){
+        res.status(400).json(err.message);
+    }
+});
 
 requestRouter.get('/user', async(req,res)=> {
     
